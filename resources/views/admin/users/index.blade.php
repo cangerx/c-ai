73192d068bf6a6144a09bc5d5d1db40aada7abc6
canger <canger@Mac-Pro.local>
2026-05-16 14:24:57 +0800

    fix: 用户编辑弹窗 z-index 修复，不再被头部遮挡

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -94,7 +94,7 @@
 
     <!-- Edit User Modal -->
     <div x-data="editUserModal()" @edit-user.window="open($event.detail)"
-         x-show="show" x-cloak style="display:none;">
+         x-show="show" x-cloak style="display:none; position:fixed; inset:0; z-index:9999;">
         <div class="modal-backdrop" @click.self="show = false"
              x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
              x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
